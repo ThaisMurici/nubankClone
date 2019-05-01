614ab8abe4e9191e3b53b838bd81b06f3d427285
Thais Moraes <murici.thais@gmail.com>
2019-05-01 14:47:20 -0300

    Add small animations

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -7,9 +7,16 @@ import {
   Container, Code, Nav, NavItem, NavText, SignOutButton, SignOutButtonText,
 } from './styles';
 
-export default function Menu() {
+export default function Menu({ translateY }) {
   return (
-    <Container>
+    <Container
+      style={{
+        opacity: translateY.interpolate({
+          inputRange: [0, 150],
+          outputRange: [0, 1],
+        }),
+      }}
+    >
       <Code>
         <QRCode value="https://rocketseat.com.br" size={80} bgColor="#8b10ae" fgColor="#fff" />
       </Code>
--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
+import { Animated } from 'react-native';
+import { PanGestureHandler, State } from 'react-native-gesture-handler';
+
 import Header from '~/components/Header';
 import Menu from '~/components/Menu';
 import Tabs from '~/components/Tabs';
@@ -18,31 +21,63 @@ import {
 } from './styles';
 
 export default function Main() {
+  const translateY = new Animated.Value(0);
+
+  const animatedEvent = Animated.event(
+    [
+      {
+        nativeEvent: {
+          translationY: translateY,
+        },
+      },
+    ],
+    { useNativeDriver: true },
+  );
+
+  function onHandlerStateChanged(event) {}
+
   return (
     <Container>
       <Header />
 
       <Content>
-        <Menu />
+        <Menu translateY={translateY} />
 
-        <Card>
-          <CardHeader>
-            <Icon name="attach-money" size={28} color="#666" />
-            <Icon name="visibility-off" size={28} color="#666" />
-          </CardHeader>
-          <CardContent>
-            <Title>Saldo disponível</Title>
-            <Description>R$ 197.611,65</Description>
-          </CardContent>
-          <CardFooter>
-            <Annotation>
-              Transferência de R$ 20,00 recebida de Diego Shell Fernandes hoje às 06:00h
-            </Annotation>
-          </CardFooter>
-        </Card>
+        <PanGestureHandler
+          onGestureEvent={animatedEvent}
+          onHandlerStateChange={onHandlerStateChanged}
+        >
+          <Card
+            style={{
+              transform: [
+                {
+                  translateY: translateY.interpolate({
+                    inputRange: [-350, 0, 380],
+                    outputRange: [-50, 0, 380],
+                    extrapolate: 'clamp',
+                  }),
+                },
+              ],
+            }}
+          >
+            <CardHeader>
+              <Icon name="attach-money" size={28} color="#666" />
+              <Icon name="visibility-off" size={28} color="#666" />
+            </CardHeader>
+            <CardContent>
+              <Title>Saldo disponível</Title>
+              <Description>R$ 197.611,65</Description>
+            </CardContent>
+            <CardFooter>
+              <Annotation>
+                Transferência de R$ 20,00 recebida de Diego Shell Fernandes hoje às 06:00h
+              </Annotation>
+            </CardFooter>
+          </Card>
+        </PanGestureHandler>
       </Content>
 
-      <Tabs />
+      <Tabs translateY={translateY} />
     </Container>
   );
 }
